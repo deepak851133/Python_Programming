175bda6e6ae7d0e339bedbe8f4b0c264ed3341eb
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2025-01-01 01:13:56 +0530

    Add files via upload

---
+++ the largest of three numbers entered by the user.py
@@ -0,0 +1,16 @@
+#5.Write a Python program that determines the largest of three numbers entered by the user.
+# Taking three numbers as input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Determining the largest number
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+# Printing the result
+print(f"The largest number is {largest}.")
