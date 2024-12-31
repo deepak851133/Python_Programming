008a0aeb0062d91ecd5e2cf7190f0b825aa4e7f8
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2024-12-31 17:57:35 +0530

    Add files via upload

---
+++ Strw1.py
@@ -0,0 +1,20 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: C:/Users/HP/OneDrive/Desktop/fileEx.py ===============
+Traceback (most recent call last):
+  File "C:/Users/HP/OneDrive/Desktop/fileEx.py", line 1, in <module>
+    f=open("f1.txt","W")
+ValueError: invalid mode: 'W'
+>>> 
+================ RESTART: C:/Users/HP/OneDrive/Desktop/fileEx.py ===============
+Traceback (most recent call last):
+  File "C:/Users/HP/OneDrive/Desktop/fileEx.py", line 1, in <module>
+    f=open("f1.txt","W")
+ValueError: invalid mode: 'W'
+>>> 
+================ RESTART: C:/Users/HP/OneDrive/Desktop/fileEx.py ===============
+Traceback (most recent call last):
+  File "C:/Users/HP/OneDrive/Desktop/fileEx.py", line 1, in <module>
+    f=open("f1.txt",'W')
+ValueError: invalid mode: 'W'
