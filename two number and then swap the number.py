b3be60f7114e4bf0b226c5cc7c9bed21136aa585
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2024-12-31 23:54:33 +0530

    Add files via upload

---
+++ two number and then swap the number.py
@@ -0,0 +1,11 @@
+# Taking two numbers as input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Swapping the numbers
+num1, num2 = num2, num1
+
+# Printing the swapped numbers
+print("After swapping:")
+print("First number:", num1)
+print("Second number:", num2)
