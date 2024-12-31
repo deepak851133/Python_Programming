c48fe6d5cb0875b1093a905b10179552b3dd549e
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2024-12-31 23:47:09 +0530

    Add files via upload

---
+++ string, int and float input .py
@@ -0,0 +1,15 @@
+#Q5. Write a code for string, int and float input ?
+# Taking a string input
+string_input = input("Enter a string: ")
+
+# Taking an integer input
+int_input = int(input("Enter an integer: "))
+
+# Taking a floating-point input
+float_input = float(input("Enter a float number: "))
+
+# Printing the inputs
+print("\nYou entered:")
+print("String:", string_input)
+print("Integer:", int_input)
+print("Float:", float_input)
