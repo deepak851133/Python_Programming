9b420caa1564652cc41de7a55fe1f58bd62b9fa0
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2025-01-01 01:39:30 +0530

    Add files via upload

---
+++ Write a program for assignment operators.py
@@ -0,0 +1,17 @@
+#Q2: Write a program for assignment operators.
+x = int(input("Enter an ini al value for x: "))
+print("\nIni al value of x:", x)
+x += 5
+print("A er x += 5, x =", x) 
+x -= 3   
+print("A er x -= 3, x =", x) 
+x *= 2
+print("A er x *= 2, x =", x) 
+x /= 4   
+print("A er x /= 4, x =", x)
+x //= 2
+print("A er x //= 2, x =", x)
+x %= 3
+print("A er x %= 3, x =", x)
+x **= 2
+print("A er x **= 2, x =", x) 
