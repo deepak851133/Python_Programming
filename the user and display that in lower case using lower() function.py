f677c85b90626e999e5c55ba4224e76cc708c5fa
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2025-01-01 01:23:48 +0530

    Add files via upload

---
+++ the user and display that in lower case using lower() function.py
@@ -0,0 +1,9 @@
+#4.Accept a name from the user and display that in lower case using lower() function
+# Accept a name from the user
+name = input("Enter your name: ")
+
+# Convert the name to lowercase
+lowercase_name = name.lower()
+
+# Display the name in lowercase
+print(f"The name in lowercase is: {lowercase_name}")
