7b71bd719dd3ff15a99e2fcd384a75e30a43aa2c
Deepak Kumar <144972002+deepak851133@users.noreply.github.com>
2025-01-01 01:36:53 +0530

    Add files via upload

---
+++ Write a program for arithma c operators.py
@@ -0,0 +1,10 @@
+#Q1:Write a program for arithma c operators
+a = int(input("Enter 1st Number: "))
+b = int(input("Enter 2nd Number: ")) 
+c = a + b
+print("Add:",c)
+print("Sub:", a - b) 
+print("Mul plica on:", a * b) 
+print("Division:",a/b) 
+print("Mod:",a%b) 
+
